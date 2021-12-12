d9a3a20c16009fedc33093e4bab806449fe87a4f
uzair <'uzairanwar2299@gmail.com'>
2021-12-12 13:40:48 +0500

    Cell Padding

--- WpfFinesse/Tabs.xaml.cs
+++ WpfFinesse/Tabs.xaml.cs
@@ -34,7 +34,7 @@ namespace WpfFinesse
             users.Add(new User() { Id = 3, FirrstName = "John Doe", LastName = "John Doe", Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
             users.Add(new User() { Id = 1, FirrstName = "John Doe", LastName = "John Doe", Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
             users.Add(new User() { Id = 2, FirrstName = "John Doe", LastName = "John Doe", Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
-            users.Add(new User() { Id = 3, FirrstName = "John DoeJohn DoeJohn Doe", LastName = "John Doe", Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+            users.Add(new User() { Id = 3, FirrstName = "John", LastName = "John Doe", Name = "Sammy", Birthday = new DateTime(1991, 9, 2) });
             users.Add(new User() { Id = 1, FirrstName = "John Doe", LastName = "John Doe", Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
             users.Add(new User() { Id = 2, FirrstName = "John Doe", LastName = "John Doe", Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
             users.Add(new User() { Id = 3, FirrstName = "John Doe", LastName = "John Doe", Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
@@ -42,7 +42,7 @@ namespace WpfFinesse
             users.Add(new User() { Id = 2, FirrstName = "John Doe", LastName = "John Doe", Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
             users.Add(new User() { Id = 3, FirrstName = "John Doe", LastName = "John Doe", Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
 
-
+            DataGridCell
             dgSimple.ItemsSource = users;
         }
         private void btnCallVaribles_Click(object sender, RoutedEventArgs e)
