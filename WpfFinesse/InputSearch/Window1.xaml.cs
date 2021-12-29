190cacbc9fe62e215d9023edb9362a82a37e6be8
uzair <'uzairanwar2299@gmail.com'>
2021-12-29 14:10:13 +0500

    phone book

---
+++ WpfFinesse/InputSearch/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfFinesse.InputSearch
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class TextInputToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            // Always test MultiValueConverter inputs for non-null
+            // (to avoid crash bugs for views in the designer)
+            if (values[0] is bool && values[1] is bool)
+            {
+                bool hasText = !(bool)values[0];
+                bool hasFocus = (bool)values[1];
+
+                if (hasFocus || hasText)
+                    return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfFinesse/WPFTimer/TimerWindow.xaml.cs
+++ WpfFinesse/WPFTimer/TimerWindow.xaml.cs
@@ -291,14 +291,14 @@ namespace WpfFinesse.WPFTimer
         //    string name = b.Name;
         //    if (name == "btnConsultCallVaribles")
         //    {
-        //        if (ConsultCallVariablePanel.Visibility == Visibility.Visible)
-        //        {
-        //            ConsultCallVariablePanel.Visibility = Visibility.Collapsed;
-        //        }
-        //        else
-        //        {
-        //            ConsultCallVariablePanel.Visibility = Visibility.Visible;
-        //        }
+        //        //if (ConsultCallVariablePanel.Visibility == Visibility.Visible)
+        //        //{
+        //        //    ConsultCallVariablePanel.Visibility = Visibility.Collapsed;
+        //        //}
+        //        //else
+        //        //{
+        //        //    ConsultCallVariablePanel.Visibility = Visibility.Visible;
+        //        //}
         //    }
         //    else if (name == "btnCallVaribles")
         //    {
@@ -850,6 +850,32 @@ namespace WpfFinesse.WPFTimer
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class TextInputToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            // Always test MultiValueConverter inputs for non-null
+            // (to avoid crash bugs for views in the designer)
+            if (values[0] is bool && values[1] is bool)
+            {
+                bool hasText = !(bool)values[0];
+                bool hasFocus = (bool)values[1];
+
+                if (hasFocus || hasText)
+                    return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
